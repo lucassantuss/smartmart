32290982ab4290c69154f183ef0a98ae73be72ee
Lucas Araújo dos Santos <lucassantuss@outlook.com>
2023-04-16 19:55:17 -0300

    Criação do repositório

---
+++ Projeto Interdisciplinar/Aplicação/Projeto_Interdisciplinar/DAO/ConexaoDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Interdisciplinar.DAO
+{
+    public class ConexaoDAO
+    {
+        /// <summary>
+        /// Método Estático que retorna um conexao aberta com o BD
+        /// </summary>
+        /// <returns>Conexão aberta</returns>
+        public static SqlConnection GetConexao()
+        {
+            // string strCon = "Data Source=LOCALHOST\\SQLEXPRESS; Database=db_Curriculo; user id=sa; password=123456";
+            string strCon = "Data Source=LOCALHOST; Database=Projeto_Interdisciplinar; user id=sa; password=123456";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Projeto_Interdisciplinar
- Projeto Interdisciplinar realizado na Faculdade Engenheiro Salvador Arena (FESA) no 5 Semestre de Engenharia de Computação
+# Projeto Interdisciplinar
+
+Projeto Interdisciplinar realizado na Faculdade Engenheiro Salvador Arena (FESA) no 5 Semestre de Engenharia de Computação
+
+## Integrantes do Grupo:
+
+Nome: Danilo Rodrigues Dantas -> RA: 081210027
+Nome: Lucas Araújo dos Santos -> RA: 081210009 
+Nome: Maik Soares Luiz        -> RA: 081210040
+Nome: Renan César de Araujo   -> RA: 081210033
